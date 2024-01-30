1a705b0d1f006658412eeda4c5ae8f6ccbd2e045
Flavia Lopes <lalmeida.flavia@gmail.com>
2024-01-30 14:54:22 -0500

    commit inicial

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Cabecalho from './components/Cabecalho.vue';
+</script>
+
+<template>
+  <div class="container">
+    <Cabecalho />
+  </div>
+</template>
+
+<style scoped>
+  .container{
+    max-width: 1280px;
+    width: 100%;
+    margin: 0 auto;
+  }
+</style>
---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,18 @@
+
+<template>
+    <header class="cabecalho">
+        <h1 class="titulo">Calculadora Aritmetica</h1>
+    </header>
+</template>
+
+<style scoped>
+    .cabecalho{
+        background-color: rgb(44, 6, 87);
+    }
+
+    .titulo{
+        padding: 24px;
+        text-align: center;
+        color: rgb(237, 237, 247);
+    }
+</style>
